0bde032644a1a15a6ed72342ee7e6019d7627db7
AnnBolkunova <105433038+AnnBolkunova@users.noreply.github.com>
2022-06-21 20:49:53 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,3 @@
-// Добавление массива из 6-ти карточек из "коробки" при загрузке страницы
-const initialCards = [
-    {
-        name: 'Армения',
-        link: 'https://images.unsplash.com/photo-1608719452475-6492c367cbc8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8JUQwJUIwJUQxJTgwJUQwJUJDJUQwJUI1JUQwJUJEJUQwJUI4JUQxJThGfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://images.unsplash.com/photo-1645128187991-3030aa113532?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=626&q=80'
-    },
-    {
-        name: 'Алтай',
-        link: 'https://images.unsplash.com/photo-1634876371724-82860814ad94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-    },
-    {
-        name: 'Карелия',
-        link: 'https://images.unsplash.com/photo-1575582293156-7d185b60c7bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80'
-    },
-    {
-        name: 'Мурманская область',
-        link: 'https://images.unsplash.com/photo-1624719635330-c03b1ed0c02a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8JUQwJUJDJUQxJTgzJUQxJTgwJUQwJUJDJUQwJUIwJUQwJUJEJUQxJTgxJUQwJUJBfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-        name: 'Озеро Байкал',
-        link: 'https://images.unsplash.com/photo-1610984660607-90b67b18e2c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fCVEMCVCMSVEMCVCMCVEMCVCOSVEMCVCQSVEMCVCMCVEMCVCQnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
-    }
-];
-
-
 // Определение элементов попапа редактирования профиля в структуре DOM
 const popupProfile = document.querySelector('.popup_type_edit-profile');
 const closeButtonProfile = popupProfile.querySelector('.popup__close-button');
@@ -38,32 +9,26 @@ const profileAuthor = profile.querySelector('.profile__title');
 const profileComment = profile.querySelector('.profile__subtitle');
 const editButton = profile.querySelector('.profile__edit-button');
 
-// Функция открытия попапа и слушатель для события
-function openPopupProfile() {
+// Слушатель события с функцией открытия попапа редактирования профиля
+editButton.addEventListener('click', openPopup => {
     popupProfile.classList.add('popup_opened');
-    nameInput.value = profileAuthor.textContent;
-    jobInput.value = profileComment.textContent;
-};
-
-editButton.addEventListener('click', openPopupProfile);
+});
 
 // Функция присвоения значений полям ввода в форме
 function formSubmitHandlerProfile(evt) {
     evt.preventDefault();
     profileAuthor.textContent = nameInput.value;
     profileComment.textContent = jobInput.value;
-    closePopupProfile();
+    popupProfile.classList.remove('popup_opened');
 };
 
 // Обработчик кнопки Сохранить
 formElementProfile.addEventListener('submit', formSubmitHandlerProfile);
 
-// Функция закрытия попапа и слушатель для события
-function closePopupProfile() {
+// Слушатель события с функцией закрытия попапа
+closeButtonProfile.addEventListener('click', closePopup => {
     popupProfile.classList.remove('popup_opened');
-};
-
-closeButtonProfile.addEventListener('click', closePopupProfile);
+});
 
 
 
@@ -77,17 +42,15 @@ const cardUrlInput = formElementCard.querySelector('#place_url');
 const addButton = profile.querySelector('.profile__add-button');
 const saveButtonCard = formElementCard.querySelector('.popup__submit-button');
 
-// Функция открытия попапа и слушатель для события
-function openPopupCard() {
+// Слушатель события с функцией открытия попапа формы добавления карточки
+addButton.addEventListener('click', openPopup => {
     popupElementCard.classList.add('popup_opened');
-};
+});
 
-addButton.addEventListener('click', openPopupCard);
-
-// Функция закрытия попапа создания карточки
-function closePopupCard() {
+// Слушатель события с функцией закрытия попапа
+closeButtonCard.addEventListener('click', closePopup => {
     popupElementCard.classList.remove('popup_opened');
-};
+});
 
 
 // Функция создания новой карточки
@@ -108,10 +71,9 @@ function formSubmitHandlerCard(evt) {
 };
 
 
-// Слушатели для кнопки submit и кнопки закрытия формы карточки
+// Слушатель для кнопки submit
 popupElementCard.addEventListener('submit', formSubmitHandlerCard);
 
-closeButtonCard.addEventListener('click', closePopupCard);
 
 
 
@@ -124,16 +86,12 @@ const imagePhoto = figureElement.querySelector('.popup__image');
 const imageCaption = figureElement.querySelector('.popup__caption');
 
 
-// Слушатель для кнопки закрытия модального окна с изображением и функции открытия и закрытия
-closeButtonImage.addEventListener('click', closePupupImage);
-
-function openPopupImage() {
-    popupElementImage.classList.add('popup_opened');
-};
-
-function closePupupImage() {
+// Слушатель для кнопки закрытия модального окна с изображением и функции закрытия
+closeButtonImage.addEventListener('click', closePupup => {
     popupElementImage.classList.remove('popup_opened');
-};
+});
+
+
 
 
 
@@ -168,7 +126,7 @@ function createCard(element) {
 
 
     cardImage.addEventListener('click', (evt) => {
-        openPopupImage();
+        popupElementImage.classList.add('popup_opened');
         imagePhoto.src = element.link;
         imagePhoto.alt = element.name;
         imageCaption.textContent = element.name;
---
+++ scripts/initial-cards.js
@@ -0,0 +1,27 @@
+// Добавление массива из 6-ти карточек из "коробки" при загрузке страницы
+const initialCards = [
+    {
+        name: 'Армения',
+        link: 'https://images.unsplash.com/photo-1608719452475-6492c367cbc8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8JUQwJUIwJUQxJTgwJUQwJUJDJUQwJUI1JUQwJUJEJUQwJUI4JUQxJThGfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://images.unsplash.com/photo-1645128187991-3030aa113532?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=626&q=80'
+    },
+    {
+        name: 'Алтай',
+        link: 'https://images.unsplash.com/photo-1634876371724-82860814ad94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+    },
+    {
+        name: 'Карелия',
+        link: 'https://images.unsplash.com/photo-1575582293156-7d185b60c7bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80'
+    },
+    {
+        name: 'Мурманская область',
+        link: 'https://images.unsplash.com/photo-1624719635330-c03b1ed0c02a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8JUQwJUJDJUQxJTgzJUQxJTgwJUQwJUJDJUQwJUIwJUQwJUJEJUQxJTgxJUQwJUJBfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+        name: 'Озеро Байкал',
+        link: 'https://images.unsplash.com/photo-1610984660607-90b67b18e2c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fCVEMCVCMSVEMCVCMCVEMCVCOSVEMCVCQSVEMCVCMCVEMCVCQnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+    }
+];
